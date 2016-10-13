aa2cb10a49093618d16d6f8c21c610798fd0fcbe
Danail Alexiev <dalexiev@Danails-MacBook-Pro.local>
2016-10-13 12:04:22 +0300

    Added logic to only notify if there are actual results from the DB operations.

--- bender/src/main/java/bg/dalexiev/bender/content/BenderContentResolver.java
+++ bender/src/main/java/bg/dalexiev/bender/content/BenderContentResolver.java
@@ -9,7 +9,7 @@ import bg.dalexiev.bender.util.Preconditions;
  * A wrapper around the default {@link ContentResolver} that adds the Bender syntax sugar.
  *
  * @author danail.alexiev
- * @since 1.1.2
+ * @since 1.1.3
  */
 public class BenderContentResolver {
 
